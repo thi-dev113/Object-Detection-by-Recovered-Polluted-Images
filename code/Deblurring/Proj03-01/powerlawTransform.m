b223c285627b68abd4f4fa33397834d46d8e2e45
WilsNTHU <wilsonlin001@gmail.com>
2024-12-03 14:10:56 +0800

    Remove Proj folder and push progress on deblurring, not completed

--- code/Deblurring/Proj03-01/powerlawTransform.m
+++
@@ -1,9 +0,0 @@
-function output = powerlawTransform(input, c, r)
-img = double(input);
-transformedImg = c * (img .^ r);
-max_element = max(transformedImg(:));
-min_element = min(transformedImg(:));
-epsilon = 1e-10;
-output = single((transformedImg - min_element) / (max_element - min_element + epsilon));
-end
-
